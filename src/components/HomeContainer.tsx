66bd76dc51160b3829b71bf8fa31cd38af9322dc
Patrick Yang <patrick.yang@goanimate.com>
2022-04-28 11:27:20 +0800

    chore: update .vscode/settings and prettier setting

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Home from './Home'
 
-type Props = {onMobileMenuClick: Function}
+type Props = { onMobileMenuClick: Function }
 
 // eslint-disable-next-line no-empty-pattern
 export default function HomeContainer({ onMobileMenuClick }: Props) {
-  return (
-    <Home onMobileMenuClick={onMobileMenuClick} />
-  )
+  return <Home onMobileMenuClick={onMobileMenuClick} />
 }
