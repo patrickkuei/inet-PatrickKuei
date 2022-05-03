258f80ccb84f14f86f8de588ae6c099faf30b4d6
Patrick Yang <patrick.yang@goanimate.com>
2022-05-03 12:50:02 +0800

    fix: button text too long will wrap

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -3,24 +3,24 @@ import React from 'react'
 const buttonClassNameMap = {
   small: {
     filled:
-      'h-10 px-8 border-2 rounded-3xl border-primary-500 text-primary-500',
+      'h-10 px-8 border-2 rounded-3xl border-primary-500 text-primary-500 whitespace-nowrap',
     elevated: '',
     outline:
-      'h-10 px-8 border-2 rounded-3xl bg-primary-500 border-primary-500 text-white',
+      'h-10 px-8 border-2 rounded-3xl bg-primary-500 border-primary-500 text-white whitespace-nowrap',
   },
   medium: {
     filled:
-      'h-10 px-8 border-2 rounded-3xl border-primary-500 text-primary-500',
+      'h-10 px-8 border-2 rounded-3xl border-primary-500 text-primary-500 whitespace-nowrap',
     elevated: '',
     outline:
-      'h-10 px-8 border-2 rounded-3xl bg-primary-500 border-primary-500 text-white',
+      'h-10 px-8 border-2 rounded-3xl bg-primary-500 border-primary-500 text-white whitespace-nowrap',
   },
   large: {
     filled:
-      'h-10 px-8 border-2 rounded-3xl border-primary-500 text-primary-500',
+      'h-10 px-8 border-2 rounded-3xl border-primary-500 text-primary-500 whitespace-nowrap',
     elevated: '',
     outline:
-      'h-10 px-8 border-2 rounded-3xl bg-primary-500 border-primary-500 text-white',
+      'h-10 px-8 border-2 rounded-3xl bg-primary-500 border-primary-500 text-white whitespace-nowrap',
   },
 }
 
