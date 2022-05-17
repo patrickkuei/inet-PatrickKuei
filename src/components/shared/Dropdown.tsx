45d5edeebd0fdff8fc799017d64918b86ab2b04c
Patrick Yang <patrick.yang@goanimate.com>
2022-05-17 16:24:05 +0800

    feat: control dropdown display outside component

--- src/components/shared/Dropdown.tsx
+++ src/components/shared/Dropdown.tsx
@@ -2,21 +2,25 @@ import { nanoid } from 'nanoid'
 import React, { ReactElement } from 'react'
 
 type Props = {
+  customClassName?: String
   overlay: Array<any>
   DropdownToggle: ReactElement
   isDropdownOpen: Boolean
 }
 
 export default function Dropdown({
+  customClassName = '',
   overlay,
   DropdownToggle,
   isDropdownOpen,
 }: Props) {
   return (
-    <div className="relative desktop:hidden">
+    <div className={`relative ${customClassName}`}>
       {DropdownToggle}
       {isDropdownOpen && (
-        <div className="bg-white shadow-drop rounded-lg absolute -right-6 desktop:hidden">
+        <div
+          className={`bg-white shadow-drop rounded-lg absolute -right-6 ${customClassName}`}
+        >
           <ul>
             {overlay.map((item) => (
               <li key={nanoid()}>{item}</li>
@@ -26,4 +30,8 @@ export default function Dropdown({
       )}
     </div>
   )
+}
+
+Dropdown.defaultProps = {
+  customClassName: '',
 }
