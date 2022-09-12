14fb8ab5a975bce41ee19478b0d2b274a01fb91d
Patrick Yang <patrick.yang@goanimate.com>
2022-09-12 15:44:59 +0800

    fix: move react-redux Provider into App.tsx to fix test root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import './App.css'
+import { store } from './app/store'
 import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Home />
+      </div>
+    </Provider>
   )
 }
 
