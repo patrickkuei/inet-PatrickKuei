ae6df8609853a69f8f4bac0829ccde76e10f79bf
Patrick Yang <patrick.yang@goanimate.com>
2022-10-14 12:57:36 +0800

    feat: add react-router

--- src/components/pages/HomeContent.tsx
+++ src/components/pages/HomeContent.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  Outlet,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 import ArticleCategorySection from './ArticleCategorySection'
 import ArticleSectionContainer from './ArticleSectionContainer'
-
 type Props = {}
 
 export default function HomeContent({}: Props) {
-  return (
-    <div className="flex desktop:my-8 desktop:mx-16 desktop-lg:mx-36">
-      <ArticleCategorySection />
-      <ArticleSectionContainer />
-    </div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <div className="flex desktop:my-8 desktop:mx-16 desktop-lg:mx-36">
+          <ArticleCategorySection />
+          <Outlet />
+        </div>
+      ),
+      children: [
+        {
+          index: true,
+          element: <ArticleSectionContainer />,
+        },
+      ],
+    },
+  ])
+
+  return <RouterProvider router={router} />
 }
