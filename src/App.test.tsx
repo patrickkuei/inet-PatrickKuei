aa73bde31670e8005680817cbd78037a6d9ff44e
abemscac <abe.msc.ac@gmail.com>
2022-10-19 21:45:30 +0800

    fix: react-router in jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
+test('renders App', () => {
+  render(<App />, { wrapper: BrowserRouter })
   expect(screen).toBeDefined()
 })
