1c532f89c6d43ba3e27abe09f00132024aef8497
Patrick Yang <patrick.yang@goanimate.com>
2022-10-27 17:07:14 +0800

    fix: try to fix duplicate div

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavbarContainer from './components/layout/NavbarContainer'
 
 export default function Layout() {
   return (
     <div className="w-screen min-w-94 h-screen min-h-screen desktop:bg-background-white">
+      <NavbarContainer />
       <Outlet />
     </div>
   )
--- src/pages/HomeContainer.tsx
+++ src/pages/HomeContainer.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import HomeContent from '../components/home/HomeContent'
-import NavbarContainer from '../components/layout/NavbarContainer'
 
 export default function HomeContainer() {
-  return (
-    <div className="w-screen min-w-94 h-screen min-h-screen desktop:bg-background-white">
-      <NavbarContainer />
-      <HomeContent />
-    </div>
-  )
+  return <HomeContent />
 }
