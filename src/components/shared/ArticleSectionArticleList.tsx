6bbc45c622d7d860806c7fbcc202daf1bccb519a
Patrick Yang <patrick.yang@goanimate.com>
2023-02-25 20:32:00 +0800

    feat: integrate pagination with article api

--- src/components/shared/ArticleSectionArticleList.tsx
+++ src/components/shared/ArticleSectionArticleList.tsx
@@ -1,21 +1,54 @@
 import moment from 'moment'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { IArticleViewModel } from '../../services/types/articles/i-article.view-model'
 import ArticleThumbnail from './ArticleThumbnail'
 import PaginationSection from './PaginationSection'
 
-type Props = {
-  articles: IArticleViewModel[]
-}
+import { useAppSelector } from '../../app/hooks'
+import { useGetArticlesQuery } from '../../services/apiSlice'
+import { ArticleCreatedWithin } from '../../services/types/articles/i-get-articles.query'
+import { Pagination } from '../../services/types/shared/pagination'
 
-const ArticleSectionArticleList = ({ articles }: Props) => {
+type Props = {}
+
+const ArticleSectionArticleList = ({}: Props) => {
+  const [page, setPage] = useState(0)
+  const [limit, setLimit] = useState(10)
   const currentMoment = useRef(moment())
+  const createdWithin = useAppSelector(
+    (state) => state.articleReducer.articleCreatedWithin,
+  )
+  const {
+    data: response,
+    error,
+    isFetching,
+  } = useGetArticlesQuery(
+    createdWithin === ArticleCreatedWithin.All
+      ? { pagination: Pagination.Basic, page, limit }
+      : {
+          pagination: Pagination.Basic,
+          page,
+          limit,
+          createdWithin,
+        },
+  )
 
-  return articles.length < 1 ? (
+  const handlePageClick = (page: number) => {
+    setPage(page)
+  }
+
+  const handleLimitChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setLimit(parseInt(event.target.value))
+    setPage(0)
+  }
+
+  return isFetching ? (
+    <div>...is loading</div>
+  ) : response!.data.length < 1 ? (
     <span className="text-tiny text-gray-500">no article</span>
   ) : (
     <>
-      {articles.map((article) => {
+      {response!.data.map((article) => {
         const createdAtDuration = moment
           .duration(-currentMoment.current.diff(moment(article.createdAt)))
           .humanize(true)
@@ -28,7 +61,13 @@ const ArticleSectionArticleList = ({ articles }: Props) => {
           />
         )
       })}
-      <PaginationSection />
+      <PaginationSection
+        currentPage={page}
+        totalPages={response!.totalPages}
+        onClick={handlePageClick}
+        onLimitChange={handleLimitChange}
+        currentLimit={limit}
+      />
     </>
   )
 }
