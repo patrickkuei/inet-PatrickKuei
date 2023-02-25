3a58d7af63fa482dcf441f04aeffe5017db57864
Patrick Yang <patrick.yang@goanimate.com>
2023-02-25 20:32:00 +0800

    feat: integrate pagination with article api

--- src/components/shared/ArticleSection.tsx
+++ src/components/shared/ArticleSection.tsx
@@ -1,29 +1,11 @@
 import clsx from 'clsx'
 import React from 'react'
-import { useAppSelector } from '../../app/hooks'
-import { useGetArticlesQuery } from '../../services/apiSlice'
-import { ArticleCreatedWithin } from '../../services/types/articles/i-get-articles.query'
-import { Pagination } from '../../services/types/shared/pagination'
 import ArticleSectionArticleList from './ArticleSectionArticleList'
 import ArticleSectionFiler from './ArticleSectionFilter'
 
 export default function Articles() {
-  const createdWithin = useAppSelector(
-    (state) => state.articleReducer.articleCreatedWithin,
-  )
-  const { data, error, isLoading } = useGetArticlesQuery(
-    createdWithin === ArticleCreatedWithin.All
-      ? { pagination: Pagination.Basic, page: 0, limit: 30 }
-      : {
-          pagination: Pagination.Basic,
-          page: 0,
-          limit: 30,
-          createdWithin,
-        },
-  )
-
   return (
-    <div className="p-6 grow min-w-0 max-w-180 desktop:bg-white desktop:mx-12 desktop:py-6 desktop:px-8 desktop:rounded-lg">
+    <div className="flex flex-col p-6 grow min-w-0 max-w-180 desktop:bg-white desktop:mx-12 desktop:py-6 desktop:px-8 desktop:rounded-lg">
       <div className="text-mobile5xl text-left font-bold desktop:text-5xl">
         Most Popular
       </div>
@@ -31,11 +13,7 @@ export default function Articles() {
         <ArticleSectionFiler />
       </div>
       <div className={clsx('flex flex-col py-6 gap-5', false && 'border-t')}>
-        {isLoading ? (
-          <div>...is loading</div>
-        ) : (
-          <ArticleSectionArticleList articles={data!.data} />
-        )}
+        <ArticleSectionArticleList />
       </div>
     </div>
   )
--- src/components/shared/PaginationSection.tsx
+++ src/components/shared/PaginationSection.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Button from './Button'
 
-type Props = {}
+type Props = {
+  currentPage: number
+  totalPages: number
+  currentLimit: number
+  onClick: (page: number) => void
+  onLimitChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
+}
 
-const PaginationSection = ({}: Props) => {
-  const pages = [1, 2, 3, 4, 5]
+const limit = [10, 15, 20, 25, 30]
+
+const PaginationSection = ({
+  totalPages,
+  onClick,
+  currentPage,
+  onLimitChange,
+  currentLimit,
+}: Props) => {
+  const pages = useMemo(
+    () => Array.from({ length: totalPages }, (_, idx) => idx),
+    [totalPages],
+  )
 
   return (
     <div className="mt-5 flex flex-row gap-2 justify-end">
+      <select
+        className="w-12 text-center"
+        onChange={onLimitChange}
+        value={currentLimit}
+      >
+        {limit.map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
       {pages.map((page) => (
         <Button
           key={page}
-          title={page.toString()}
-          fillType="outline"
+          title={(page + 1).toString()}
+          fillType={currentPage === page ? 'filled' : 'outline'}
           size="small"
+          onClick={() => onClick(page)}
         />
       ))}
     </div>
