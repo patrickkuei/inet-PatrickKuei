3d4164799dc9d7155e4f027d4b928ff302e5d53a
Patrick Yang <patrick.yang@goanimate.com>
2023-02-26 15:58:33 +0800

    fix: article category section is now sticky when scroll down

--- src/components/shared/ArticleCategorySection.tsx
+++ src/components/shared/ArticleCategorySection.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function ArticleCategorySection({}: Props) {
   return (
-    <div className="hidden w-52 p-4 bg-white rounded-lg text-left desktop:block shrink-0 self-start">
+    <div className="sticky top-5 hidden w-52 p-4 bg-white rounded-lg text-left desktop:block shrink-0 self-start">
       <AriticleCagegory categories={articleCategories} />
     </div>
   )
