e05f6f0f11238d5ca5f0b1c5dda2a1cf8ed1b3b8
Patrick Yang <patrick.yang@goanimate.com>
2023-02-26 18:55:58 +0800

    feat: add article section title to handle different title when switch category

--- src/components/shared/ArticleSection.tsx
+++ src/components/shared/ArticleSection.tsx
@@ -2,17 +2,18 @@ import { useAppSelector } from '@inet/app/hooks'
 import clsx from 'clsx'
 
 import ArticleSectionArticleList from './ArticleSectionArticleList'
-import ArticleSectionFiler from './ArticleSectionFilter'
+import ArticleSectionTitle from './ArticleSectionTitle'
 
 export default function Articles() {
+  const {
+    code: category,
+    id,
+    imageUrl,
+  } = useAppSelector((state) => state.articleReducer.currentCategory)
+
   return (
     <div className="flex flex-col p-6 grow min-w-0 max-w-180 desktop:bg-white desktop:mx-12 desktop:py-6 desktop:px-8 desktop:rounded-lg">
-      <div className="text-mobile5xl text-left font-bold desktop:text-5xl">
-        Most Popular
-      </div>
-      <div className="flex mt-4 mb-5 gap-4 flex-wrap">
-        <ArticleSectionFiler />
-      </div>
+      <ArticleSectionTitle category={category} imageUrl={imageUrl} />
       <div className={clsx('flex flex-col py-6 gap-5', false && 'border-t')}>
         <ArticleSectionArticleList />
       </div>
