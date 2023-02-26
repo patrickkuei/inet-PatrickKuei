e6956ff35eb24f6b5edade039940ecc9087aab24
Patrick Yang <patrick.yang@goanimate.com>
2023-02-26 16:32:24 +0800

    refactor: move article category section into layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import NavbarContainer from './components/layout/NavbarContainer'
+import ArticleCategorySection from './components/shared/ArticleCategorySection'
 
 export default function Layout() {
   return (
     <div className="w-screen pb-9 min-w-94 desktop:bg-background-white">
       <NavbarContainer />
-      <Outlet />
+      <div className="flex desktop:my-8 desktop:mx-16 desktop-lg:mx-36">
+        <ArticleCategorySection />
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -3,10 +3,5 @@ import ArticleSectionContainer from '../shared/ArticleSectionContainer'
 type Props = {}
 
 export default function HomeContent({}: Props) {
-  return (
-    <div className="flex desktop:my-8 desktop:mx-16 desktop-lg:mx-36">
-      <ArticleCategorySection />
-      <ArticleSectionContainer />
-    </div>
-  )
+  return <ArticleSectionContainer />
 }
