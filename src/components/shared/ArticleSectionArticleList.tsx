6858497bfcd5fac862048906340f8b7bd627552f
Patrick Yang <patrick.yang@goanimate.com>
2023-02-26 22:34:23 +0800

    fix: back to page 1 after switch category

--- src/components/shared/ArticleSectionArticleList.tsx
+++ src/components/shared/ArticleSectionArticleList.tsx
@@ -3,8 +3,9 @@ import { useRef, useState } from 'react'
 import ArticleThumbnail from './ArticleThumbnail'
 import PaginationSection from './PaginationSection'
 
-import { useAppSelector } from '@inet/app/hooks'
+import { useAppDispatch, useAppSelector } from '@inet/app/hooks'
 import usePagination from '@inet/hooks/use-pagination'
+import { setPage } from '@inet/redux/slices/paginationSlice'
 import { useGetArticlesQuery } from '@inet/services/apiSlice'
 import { ArticleCreatedWithin } from '@inet/services/types/articles/i-get-articles.query'
 import { Pagination } from '@inet/services/types/shared/pagination'
@@ -12,12 +13,17 @@ import { Pagination } from '@inet/services/types/shared/pagination'
 type Props = {}
 
 const ArticleSectionArticleList = ({}: Props) => {
+  const dispatch = useAppDispatch()
+  const { limit, updateLimit } = usePagination()
   const currentMoment = useRef(moment())
   const createdWithin = useAppSelector(
     (state) => state.articleReducer.articleCreatedWithin,
   )
+  const page = useAppSelector((state) => state.paginationReducer.page)
 
-  const { page, limit, updatePage, updateLimit } = usePagination()
+  const handlePageClick = (page: number) => {
+    dispatch(setPage(page))
+  }
 
   const {
     data: response,
@@ -53,7 +59,7 @@ const ArticleSectionArticleList = ({}: Props) => {
       <PaginationSection
         currentPage={page}
         totalPages={response!.totalPages}
-        onClick={updatePage}
+        onClick={handlePageClick}
         onLimitChange={updateLimit}
         currentLimit={limit}
       />
