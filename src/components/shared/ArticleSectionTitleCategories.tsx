842128d8d636ef4b01f6d69046294a26f5d02dee
Patrick Yang <patrick.yang@goanimate.com>
2023-02-28 22:07:05 +0800

    feat: extract article section title, add search result title, no search result component

---
+++ src/components/shared/ArticleSectionTitleCategories.tsx
@@ -0,0 +1,38 @@
+import { SearchIcon } from '@inet/icons'
+import React from 'react'
+import Input from './Input'
+
+type Props = {
+  category: string
+  imageUrl: string
+}
+
+const ArticleSectionTitleCategories = ({ category, imageUrl }: Props) => {
+  return (
+    <>
+      <div className="flex flex-row mb-4 gap-6">
+        <img
+          width={80}
+          height={80}
+          src={imageUrl}
+          alt={category}
+          className="rounded-full"
+        />
+        <div className="text-left">
+          <div className="text-10.5">{category.toUpperCase()}</div>
+          <div>Articles Today: 0</div>
+        </div>
+      </div>
+      <div>
+        <Input
+          type="text"
+          className="w-64"
+          placeholder="Search in this category."
+          suffix={<SearchIcon className="fill-primary-200" />}
+        />
+      </div>
+    </>
+  )
+}
+
+export default ArticleSectionTitleCategories
