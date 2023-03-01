8bc9fe319de4c0f95e72c27d5f58c590935756d9
Patrick Yang <patrick.yang@goanimate.com>
2023-03-01 17:11:16 +0800

    fix: failed test case

--- src/components/shared/ArticlePreview.test.tsx
+++ src/components/shared/ArticlePreview.test.tsx
@@ -1,10 +1,14 @@
+import { store } from '@inet/app/store'
 import articles from '@inet/mockData/articles'
+import { Provider } from 'react-redux'
 import renderer from 'react-test-renderer'
 import ArticlePreview from './ArticlePreview'
 
 it('render ArticlePreview', () => {
   const component = renderer.create(
-    <ArticlePreview article={articles[0]} createdAt="8 days ago" />,
+    <Provider store={store}>
+      <ArticlePreview article={articles[0]} createdAt="8 days ago" />
+    </Provider>,
   )
 
   let tree = component.toJSON()
