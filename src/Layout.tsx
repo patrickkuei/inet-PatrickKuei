6de50c9852cf7f479846669713a2e10a2e277f67
Patrick Yang <patrick.yang@goanimate.com>
2023-03-07 10:32:42 +0800

    feat: add redirect to category/popular

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import NavbarContainer from './components/layout/NavbarContainer'
 import ArticleCategorySection from './components/shared/ArticleCategorySection'
+import useConditionNavigate from './hooks/use-condition-navigate'
 
 export default function Layout() {
   const location = useLocation()
+  useConditionNavigate('/', 'category/popular')
 
   return (
     <div className="w-screen pb-9 min-w-94 desktop:bg-background-white">
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import HomeContainer from './pages/HomeContainer'
 
@@ -6,10 +6,8 @@ export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomeContainer />} />
-        <Route>
-          <Route path="category/:currentCategory" element={<HomeContainer />} />
-        </Route>
+        <Route path="*" element={<Navigate to="category/popular" />} />
+        <Route path="category/:currentCategory" element={<HomeContainer />} />
         <Route path="edit" element={<>edit</>} />
         <Route path="setting" element={<>setting</>} />
       </Route>
