46fbce460aee824a7065a97fe9ea500f45596869
Patrick Yang <patrick.yang@goanimate.com>
2023-03-08 20:15:26 +0800

    refactor: move Article[different part] files into Home folder

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
-import HomeContainer from './pages/HomeContainer'
+import HomePage from './pages/HomePage'
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="*" element={<Navigate to="category/popular" />} />
-        <Route path="category/:currentCategory" element={<HomeContainer />} />
+        <Route path="category/:currentCategory" element={<HomePage />} />
         <Route path="edit" element={<>edit</>} />
         <Route path="setting" element={<>setting</>} />
       </Route>
